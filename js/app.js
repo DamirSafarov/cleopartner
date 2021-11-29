d96dcf75ee0f6dd4c68e8747b2072a06fac01823
Damir_Safarov <91431110+DamirSafarov@users.noreply.github.com>
2021-11-29 21:51:00 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,6 @@
+$('.rewiews-slider').slick({
+    prevArrow:'<button type="button" class="slick-prev"><img src="images/dest/slider-arrow_left.svg" alt="arrow"></button>',
+    nextArrow:'<button type="button" class="slick-next"><img src="images/dest/slider-arrow_right.svg" alt="arrow"></button>',
+    fade: true,
+    cssEase: 'linear'
+});
